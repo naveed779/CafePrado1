08dbc1f8d03356ac1e16902f00345bb6708cd886
Naveeed <Naveeed@DESKTOP-K8R30QD>
2018-05-13 13:25:51 +0500

    Add project files.

---
+++ CafePrado/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CafePrado.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult SignIN()
+        {
+          
+            return View();
+        }
+        public ActionResult SignUP()
+        {
+
+            return View();
+        }
+        public ActionResult TakeOrder()
+        {
+
+            return View();
+        }
+        public ActionResult CustomerPayment()
+        {
+
+            return View();
+        }
+        public ActionResult VenderOrder()
+        {
+
+            return View();
+        }
+        public ActionResult VenderPayment()
+        {
+
+            return View();
+        }
+        public ActionResult HireEmployee()
+        {
+
+            return View();
+        }
+        public ActionResult EmployeeSalary()
+        {
+
+            return View();
+        }
+        public ActionResult AddNewItems()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ CafePrado/Models/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CafePrado.Models
+{
+    public class PersonModel
+    {
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+        public int age { get; set; } = 0;   
+
+        public bool isAlive { get; set; } = true;
+    }
+}
