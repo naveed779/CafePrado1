215884673cf95403ebe53b42c17f24280e6d6115
rizwa <rizwa@DESKTOP-L4B6SHB>
2018-05-14 02:54:35 +0500

    naya ha rat ko 3 bajay banaya ha

--- CafePrado/Controllers/HomeController.cs
+++ CafePrado/Controllers/HomeController.cs
@@ -1,17 +1,20 @@
-﻿using System;
+﻿using CafePrado.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Data;
 namespace CafePrado.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
-        }
+             }
 
         public ActionResult About()
         {
@@ -37,14 +40,56 @@ namespace CafePrado.Controllers
             return View();
         }
         public ActionResult TakeOrder()
-        {
 
-            return View();
+        {
+         
+
+            return View(new TakeOrder());
         }
+        [HttpPost]
+        public ActionResult TakeOrder(TakeOrder model)
+        {
+            
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-L4B6SHB;Initial Catalog=Managment;Integrated Security=True");
+            con.Open();
+            String query = "INSERT INTO TakeOrder VALUES (@CustomerID ,@Name,@Cell,@Status)";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@CustomerID", model.CustomerID);
+            cmd.Parameters.AddWithValue("@Name", model.Name);
+            cmd.Parameters.AddWithValue("@Cell", model.Cell);
+            cmd.Parameters.AddWithValue("@Status", model.Status);
+         
+            cmd.ExecuteNonQuery();
+            
+
+            
+
+            /*TakeOrder tb1 = new TakeOrder();
+            tb1.CustomerID = model.CustomerID;
+            tb1.Name = model.Name;
+            tb1.Cell = model.Cell;
+            tb1.Status = model.Status;
+            db.Orders.Add(tb1);
+            db.SaveChanges();*/
+
+            return RedirectToAction("Index");
+
+        }
+
+
+
         public ActionResult CustomerPayment()
         {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-L4B6SHB;Initial Catalog=Managment;Integrated Security=True");
+            con.Open();
+            DataTable dt = new DataTable();
+            string query1 = "SELECT * from TakeOrder";
+            SqlDataAdapter sda = new SqlDataAdapter(query1, con);
+            sda.Fill(dt);
+            return View(dt);
 
-            return View();
+
+          
         }
         public ActionResult VenderOrder()
         {
---
+++ CafePrado/Models/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CafePrado.Models
+{
+    public class Main
+    {
+        
+            public IEnumerable<TakeOrder> TakeOrders { get; set; }
+
+
+    }
+}
